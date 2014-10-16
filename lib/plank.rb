2e8ceee1f1e2cceb2f16098a83ade7c631d8f851
Tim Green <tiimgreen@gmail.com>
2014-10-16 23:09:55 +0100

    Finish

--- lib/plank.rb
+++ lib/plank.rb
@@ -1,12 +1,23 @@
-module Plank
-  class Server
+ require 'thin'
 
-    def initialize
-      @options = default_options
-    end
+ module Plank
+  class Server
+    PORT = "9292"
+    HOST = "localhost"
+    ENVIRONMENT = "development"
 
     def self.start
       new(ARGV).start
+    end
+
+    def initialize(args)
+      @options = default_options
+      @options[:config] = args[0]
+      @app = build_app
+    end
+
+    def start
+      server.run @app, @options
     end
 
     private
@@ -18,8 +29,55 @@ module Plank
         Host: HOST
       }
     end
+
+    def build_app
+      Plank::Builder.parse_file(@options[:config])
+    end
+
+    def server
+      @server ||= Plank::Handler.default
+    end
   end
 
   class Builder
+
+    def self.parse_file(file)
+      cfg_file = File.read file
+      new_from_string cfg_file
+    end
+
+    def initialize(&block)
+      instance_eval(&block) if block_given?
+    end
+
+    def run(app)
+      @run = app
+    end
+
+    def to_app
+      @run
+    end
+
+    def self.new_from_string(builder_script)
+      eval("Plank::Builder.new {\n" + builder_script + "\n}.to_app")
+    end
+  end
+
+  module Handler
+
+    def self.default
+      Plank::Handler::Thin
+    end
+  end
+
+  class Thin
+
+    def self.run(app, options = {})
+      host = options[:Host]
+      port = options[:Port]
+      args =  [host, port, app, options]
+      server = ::Thin::Server.new(*args)
+      server.start
+    end
   end
 end
